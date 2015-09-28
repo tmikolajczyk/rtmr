cf48e4998cab779cf40dfa819090f202e543a7e9
Jowalski <jowalski@gmail.com>
2015-09-28 12:22:07 -0400

    Use caching method for timeline like task lists

--- R/timeline.r
+++ R/timeline.r
@@ -1,7 +1,18 @@
-##' Request a new timeline.
+##' Get the timeline in use or request a new one.
 ##'
 ##' @return a timeline id character string
 rtm_timeline <- function() {
+  if (!exists(".rtm_timeline", globalenv()))
+    rtm_cache_timeline()
+  get(".timeline", envir = globalenv())
+}
+
+##' Request a new timeline and store it in the global environment.
+##'
+##' @param varname the name of the global variable to store it in
+##' @param envir the environment, by default the global environment
+rtm_cache_timeline <- function(varname = ".rtm_timeline",
+                               envir = globalenv()) {
   rsp <- rtm_req("rtm.timelines.create")
-  rsp[["timeline"]]
+  assign(varname, rsp[["timeline"]], envir = envir)
 }
