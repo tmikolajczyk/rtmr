d86ee30b4d6c4bb23f27bb35208962f609f896b1
Jowalski <jowalski@gmail.com>
2015-10-03 02:30:18 -0400

    Rename functions in tests to new task method names

--- tests/testthat/test-task.R
+++ tests/testthat/test-task.R
@@ -3,7 +3,7 @@ context("Manipulating Tasks")
 test_that(
   "add a task", {
     name <- sprintf("testing %s #zzz_rtmr_test_add", sample.int(1e7, 1))
-    expect_output(rtm_add_task(name), "Added task")
+    expect_output(addTask(name), "Added task")
   })
 
 test_that(
@@ -27,14 +27,13 @@ test_that(
     r_int <- sample.int(1e7, 1)
     name <- sprintf("test task w note %s #zzz_rtmr_test_add", r_int)
     
-    expect_output(rtm_add_task(name), "Added task")
+    expect_output(addTask(name), "Added task")
     
     zzz_tasks <- rtm_search("list:zzz_rtmr_test_add")
     task <- zzz_tasks[grep(paste0(r_int), zzz_tasks[["name"]]),]
     note_title <- sprintf("test note title %s", r_int)
     note_text <- sprintf("this is a test note %s", r_int)
     
-    expect_output(apply_rtm_method("notes.add", task, note_title,
-                                   note_text = note_text),
+    expect_output(notes.add(task, note_title, note_text),
                   "Method \"notes.add\" completed")
   })
